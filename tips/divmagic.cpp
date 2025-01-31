422e2e920f3ca43bdff42d3d335ad128fe229dfe
Luana7 <53417057+qluana7@users.noreply.github.com>
2025-01-31 14:12:43 +0000

    Added signed magic number and new algorithm used for get magic number. Algorithm source : Hacker's Delight

--- tips/divmagic.cpp
+++ tips/divmagic.cpp
@@ -2,38 +2,66 @@
 
 #define i32 int
 #define u32 unsigned int
-#define u64 unsigned long long
-#define i128 __int128_t
 
 using namespace std;
 
-pair<i32, u32> __get_magic_u32(u32 d) {
-    for (int i = 32; ; i++) {
-        u64 k = 1ULL << i, m = d - k % d;
-        if (m * 4294967295 < k) {
-            u64 r = (k + m) / d;
+constexpr inline u32 __abs(i32 d)
+{ return d ^ (d >> 31) - (d >> 31); }
 
-            return { (i32)r, r <= 4294967295 ? i - 32 : -1 };
-        }
-    }
-}
+pair<pair<i32, u32>, bool> get_magic_i32(i32 d) {
+    constexpr u32 mg = 0x80000000;
+    i32 p = 31;
+    u32 delta,
+        ad = __abs(d),
+        t = mg + ((u32)d >> 31),
+        anc = t - 1 - t % ad,
+        q1 = mg / anc,
+        r1 = mg - q1 * anc,
+        q2 = mg / ad,
+        r2 = mg - q2 * ad;
+    
+    do {
+        p++;
+        
+        q1 <<= 1; r1 <<= 1;
+        if (r1 >= anc) { q1++; r1 -= anc; }
 
-pair<i32, u32> __get_magic_u32_complex(u32 d) {
-    pair<u32, u32> p = { 0, 0 };
+        q2 <<= 1; r2 <<= 1;
+        if (r2 >= ad) { q2++; r2 -= ad; }
 
-    for (int i = 0; i < 32; i++) {
-        __int128_t tmp = 1;
-        tmp = (tmp << (33 + i)) / d - (1LL << 32) + 1;
+        delta = ad - r2;
+    } while (q1 < delta || (q1 == delta && r1 == 0));
 
-        if (tmp < (1LL << 32)) p = { (i32)tmp, i };
-        else break;
-    }
-
-    return p;
+    return { { (i32)(q2 + 1) * (d < 0 ? -1 : 1), p - 32 }, false };
 }
 
 pair<pair<i32, u32>, bool> get_magic_u32(u32 d) {
-    auto [a1, b1] = __get_magic_u32(d);
-    if (b1 != -1) return { { a1, b1 }, false };
-    else return { __get_magic_u32_complex(d), true };
+    constexpr u32 mg1 = 0x80000000, mg2 = 0x7FFFFFFF;
+    i32 p = 31;
+    u32 delta,
+        nc = -1 - (-d) % d,
+        q1 = mg1 / nc,
+        r1 = mg1 - q1 * nc,
+        q2 = mg2 / d,
+        r2 = mg2 - q2 * d;
+    
+    bool complex = false;
+    
+    do {
+        p++;
+
+        if (r1 >= nc - r1) {
+            q1 = q1 * 2 + 1;
+            r1 = r1 * 2 - nc;
+        } else { q1 <<= 1; r1 <<= 1; }
+        if (r2 + 1 >= d - r2) {
+            complex = q2 >= mg2;
+            q2 = q2 * 2 + 1;
+            r2 = r2 * 2 + 1 - d;
+        } else { complex = q2 >= mg1; q2 <<= 1; r2 = r2 * 2 + 1; }
+
+        delta = d - 1 - r2;
+    } while (p < 64 && (q1 < delta || (q1 == delta && r1 == 0)));
+
+    return { { q2 + 1, p - 32 }, complex };
 }
